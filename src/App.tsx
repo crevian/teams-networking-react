3cf8df5e7a5ede09959d5e35805bafcbd299cffd
Vlad Crevian <vlad.crevian@gmail.com>
2023-08-07 16:56:39 +0300

    update from nmatei

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,10 @@
 import React from "react";
 //import logo from "./logo.svg";
-import logo from "./images/network-team-icon.png";
 import "./App.css";
 import "./style.css";
-
-function App_old() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-function AppHeader() {
-  return (
-    <header>
-      <div id="header-wrapper">
-        <div id="myPicture">
-          <img src={logo} alt="poza" width="100" />
-        </div>
-        <div id="header-info">
-          <h1>Teams Networking</h1>
-          <h2>Crud operations example (Create, Read, Update, Delete)</h2>
-        </div>
-      </div>
-    </header>
-  );
-}
-
-function ContentWrapper() {
-  return <div id="main">TODO table...</div>;
-}
-
-function AppFooter() {
-  return (
-    <footer>
-      <a
-        className="ïtem"
-        href="https://github.com/crevian/teams-networking"
-        target="_blank"
-      >
-        <span>👩‍💻</span>
-        <span>Source Code</span>
-      </a>
-      <span className="item">|</span>
-      <a
-        className="item"
-        href="https://github.com/nmatei/node-api"
-        target="_blank"
-      >
-        <span>📃</span>
-        <span>API docs</span>
-      </a>
-    </footer>
-  );
-}
+import { AppFooter } from "./footer/components";
+import AppHeader from "./header";
+import { ContentWrapper } from "./main/components";
 
 function App() {
   return (
--- src/examples.ts
+++ src/examples.ts
@@ -1,15 +1,15 @@
 let firstName = "yes";
 // firstName = false;
 
-function getConf() {
+function getConf(r) {
   return {
     name: "my conf",
     x: 123,
-    ready: true,
+    ready: r
   };
 }
 
-const conf = getConf();
+const conf = getConf(true);
 console.info(conf.name);
 
 // conf.ready = "not ready";
