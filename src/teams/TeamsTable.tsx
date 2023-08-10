1c2e353337b9fb5ca1a961b76c40feea4da1ca8d
Vlad Crevian <vlad.crevian@gmail.com>
2023-08-10 20:12:16 +0300

    extract into multiple functions on class

--- src/teams/TeamsTable.tsx
+++ src/teams/TeamsTable.tsx
@@ -341,6 +341,26 @@ export class TeamsTableWrapper extends React.Component<WrapperProps, State> {
     }
   }
 
+  async deleteTeam(id: string) {
+    this.setState({ loading: true });
+    const status = await deleteTeamRequest(id);
+    if (status.success) {
+      this.loadTeams();
+    }
+  }
+
+  inputChange(name, value) {
+    console.info("input change", value);
+    this.setState((state) => {
+      const team = { ...state.team };
+
+      team[name] = value;
+      return {
+        team,
+      };
+    });
+  }
+
   render() {
     console.info("render");
     return (
@@ -348,27 +368,14 @@ export class TeamsTableWrapper extends React.Component<WrapperProps, State> {
         loading={this.state.loading}
         teams={this.state.teams}
         team={this.state.team}
-        deleteTeam={async (id) => {
-          this.setState({ loading: true });
-          const status = await deleteTeamRequest(id);
-          if (status.success) {
-            this.loadTeams();
-          }
+        deleteTeam={(id) => {
+          this.deleteTeam(id);
         }}
         startEdit={(team) => {
-          console.info("start edit", team);
           this.setState({ team });
         }}
         inputChange={(name, value) => {
-          console.info("input change", value);
-          this.setState((state) => {
-            const team = { ...state.team };
-
-            team[name] = value;
-            return {
-              team,
-            };
-          });
+          this.inputChange(name, value);
         }}
         save={() => {
           this.save();
